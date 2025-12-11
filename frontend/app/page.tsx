4a68b183fd6b76136899b0d2606d799fa5d390ff
samarkun <samarkun4@gmail.com>
2025-12-11 07:36:40 -0500

    add shadecn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { LandingPage } from "./components/Landing";
+import LandingPage2 from "./components/LandingPage2";
+import LandingPage3 from "./components/LandingPage3";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between ">
-      <LandingPage />
-    </div>
+    <div className="flex min-h-screen flex-col items-center justify-between bg-[#05050a] text-white">
+      < LandingPage />
+      {/* <LandingPage2 /> */ }
+  {/* <LandingPage3 /> */ }
+    </div >
 
   );
 }
