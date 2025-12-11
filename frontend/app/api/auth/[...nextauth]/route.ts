a3d47c2fe3b8a720f47df0a2dd5372701346537a
samarkun <samarkun4@gmail.com>
2025-12-11 07:35:15 -0500

    for the nextauth

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -3,12 +3,18 @@ import GoogleProvider from "next-auth/providers/google";
 
 // we add onlg google provider .
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-    ]
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true
+    }
+  }
+
 })
 
 export { handler as GET, handler as POST }
