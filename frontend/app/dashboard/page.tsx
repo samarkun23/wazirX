4b5bbdb4a103ddeb74db0cc8b6d5bf06e4083914
samarkun <samarkun4@gmail.com>
2025-12-12 09:29:56 -0500

    we astract the public key and pass to the dashboardui

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,8 +1,40 @@
-'use client'
-export default function Dashboard() {
+import DashboardUi from "@/components/ui/DashboardUi";
+import { authConfig } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import db from "@/app/db"
+
+async function getUserWallet() {
+    const session = await getServerSession(authConfig); 
+
+    const userWallet = await db.solWallet.findFirst({
+        where:{
+            userId: session?.user.uid
+        },
+        select:{
+            publicKey: true
+        }
+    })
+
+    if (!userWallet) {
+        return{
+            error: "No solana wallet"
+        } 
+    }
+
+    return {error: null , userWallet};
+
+}
+
+
+export default async function Dashboard() {
+    const userWallet = await getUserWallet();
+
+    if (userWallet.error || !userWallet.userWallet?.publicKey) {
+        return <>No Solana wallet found</> 
+    }
     return (
-        <div>
-            Dashboard here . 
-        </div>
+        <DashboardUi publicKey={userWallet.userWallet?.publicKey}/>
+
     )
+    
 }
