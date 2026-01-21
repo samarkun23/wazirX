3c43b76f2e4814e2518f6a5e96e3a925f04edb0a
samarkun <samarkun4@gmail.com>
2026-01-21 11:58:56 -0500

    authentication

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -13,6 +13,11 @@ export interface session extends Session {
     }
 }
 
+console.log({
+    clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+}
+)
 export const authConfig = {
     providers: [
         GoogleProvider({
@@ -20,71 +25,77 @@ export const authConfig = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
     ],
-    callbacks: {
-        session: ({ session , token}: any): session => { 
-            const newSession: session = session as session;
-            if (newSession.user && token.uid) {
-                newSession.user.uid = token.uid ?? ""; 
-            }
-            return newSession;
-        },
-        async jwt({ token, account , profile }: any){
-            const user = await db.user.findFirst({
-                where: {
-                    sub: account?.providerAccountId ?? ""
-                }
-            })
-            if (user) {
-                token.uid = user.id 
-            }
-            return token
-        },
-        async signIn({ user, account, profile, email, credentials }: any) {
-            if (account?.provider === "google") {
-                const email = user.email;
-                if (!email) {
-                    return false
-                }
+    // callbacks: {
 
-                const userDb = await db.user.findFirst({
-                    where: {
-                        username: email
-                    }
-                })
+    //     session: ({ session, token }: any): session => {
+    //         const newSession: session = session as session;
+    //         if (newSession.user && token.uid) {
+    //             newSession.user.uid = token.uid ?? "";  //
+    //         }
+    //         return newSession;
+    //     },
 
-                if (userDb) {
-                    return true
-                }
+    //     async jwt({ token, account, profile }: any) {
+    //         if (account) {
 
-                const keypair = Keypair.generate();
-                const publicKey = keypair.publicKey.toBase58();
-                const privateKey = keypair.secretKey.toString();
+    //             const user = await db.user.findFirst({
+    //                 where: {
+    //                     sub: account?.providerAccountId ?? ""
+    //                 }
+    //             })
+    //             if (user) {
+    //                 token.uid = user.id // when id gonna create we fetch the sub our userId and put that uid in the token 
+    //             }
+    //         }
+    //         return token
+    //     },
 
-                await db.user.create({
-                    data: {
-                        username: email,
-                        name: profile?.name,
-                        //@ts-ignore
-                        profileImage: profile?.picture,
-                        provider: "Google",
-                        sub: account.providerAccountId,
-                        solWallet: {
-                            create: {
-                                publicKey: publicKey,
-                                privateKey: privateKey
-                            }
-                        },
-                        InrWallet: {
-                            create: {
-                                balance: 0
-                            }
-                        }
-                    }
-                })
+    //     async signIn({ user, account, profile, email, credentials }: any) {
+    //         if (account?.provider === "google") {
+    //             const email = user.email;
+    //             if (!email) {
+    //                 return false
+    //             }
 
-            }
-            return true
-        }
-    }
+    //             const userDb = await db.user.findFirst({
+    //                 where: {
+    //                     username: email
+    //                 }
+    //             })
+
+    //             if (userDb) {
+    //                 return true
+    //             }
+
+    //             const keypair = Keypair.generate();
+    //             const publicKey = keypair.publicKey.toBase58();
+    //             const privateKey = keypair.secretKey.toString();
+
+    //             await db.user.create({
+    //                 data: {
+    //                     username: email,
+    //                     name: profile?.name,
+    //                     //@ts-ignore
+    //                     profileImage: profile?.picture,
+    //                     provider: "Google",
+    //                     sub: account.providerAccountId, // google provide this id we store this id in db as well 
+    //                     solWallet: {
+    //                         create: {
+    //                             publicKey: publicKey,
+    //                             privateKey: privateKey
+    //                         }
+    //                     },
+    //                     InrWallet: {
+    //                         create: {
+    //                             balance: 0
+    //                         }
+    //                     }
+    //                 }
+    //             })
+
+    //         }
+    //         return true
+    //     }
+    // }
 
 }
---
+++ frontend/lib/constants.ts
@@ -0,0 +1,8 @@
+//list of token that we support
+export const SUPPORTED_TOKENS = [{
+    name: "USDC",
+    mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+}, {
+    name: "USDT",
+    mint: "Es9vMFrzaC1H6zzggBqqqEgakx4eCnmLmJtZNe5yW3sn",
+}]
