83e440aab3831e75742c9576d19ca729f313c114
samarkun <samarkun4@gmail.com>
2026-01-22 19:09:59 +0530

    create a user in db

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,61 +1,8 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-import db  from '@/app/db'
-import { Keypair } from '@solana/web3.js';
+// import db from '@/app/db'
+import { authConfig } from "@/lib/auth";
 
 // we add onlg google provider .
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-    })
-  ],
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      if(account?.provider === "google"){
-        const email = user.email;
-        if (!email) {
-          return false 
-        }
-        
-        const userDb = await db.user.findFirst({
-          where:{
-            username: email
-          }
-        })
-
-        if (userDb) {
-          return true 
-        }
-
-        const keypair = Keypair.generate();
-        const publicKey = keypair.publicKey.toBase58();
-        const privateKey = keypair.secretKey.toString();
-
-        await db.user.create({
-          data: {
-            username: email,
-            provider: "Google",
-            solWallet: {
-              create: {
-                publicKey: publicKey,
-                privateKey: privateKey
-              }
-            },
-            InrWallet: {
-              create:{
-                balance: 0
-              }
-            }
-          }
-        })
-
-      }
-      return true
-    }
-  }
-
-})
+const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST }
