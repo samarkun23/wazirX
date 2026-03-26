075c3b2c7b1bd71875fbabac8816f5fb27acda60
samarkun <samarkun4@gmail.com>
2026-03-27 00:52:07 +0530

    this is what actual get the prices of tokens and all the stuff

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,8 +1,71 @@
+import { Connection } from "@solana/web3.js";
+import axios from 'axios'
+
+let LAST_UPDATE: number | null = null;
+let prices: {
+    [key: string]: {
+        price: string;
+    }
+} = {};
+const TOKEN_PRICE_REFRESH_INTERVAL = 60 * 1000; // every 6sec 
+
+export interface TokenDetails {
+    name: string;
+    mint: string;
+    native: boolean;
+    price: string;
+    image: string
+}
 //list of token that we support
-export const SUPPORTED_TOKENS = [{
+export let SUPPORTED_TOKENS: TokenDetails[] = [{
     name: "USDC",
     mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    native: false,
+    price: "1",
+    image: "https://imgs.search.brave.com/v4GS1a84OqBD8OJyL3O11HjI341YIqGf0zxlNYx9EwY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jcnlw/dG9sb2dvcy5jYy9s/b2dvcy91c2QtY29p/bi11c2RjLWxvZ28u/cG5nP3Y9MDQw"
 }, {
     name: "USDT",
     mint: "Es9vMFrzaC1H6zzggBqqqEgakx4eCnmLmJtZNe5yW3sn",
+    native: false,
+    price: "1",
+    image: "https://imgs.search.brave.com/9jJ7VtdwP-OkbCfSx6zuv-kIMfNj30VN_KRUgrkjwEo/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jcnlw/dG9sb2dvcy5jYy9s/b2dvcy90ZXRoZXIt/dXNkdC1sb2dvLnBu/Zz92PTA0MA"
+}, {
+    name: "SOL",
+    mint: "So11111111111111111111111111111111111111112",
+    native: true,
+    price: "180",
+    image: "https://imgs.search.brave.com/n5PBnAUTmeNFhL-7_yJlJPC9VTO5_zzjsXe1kvYmguk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzY0LzIvc29sYW5h/LWxvZ28tcG5nX3Nl/ZWtsb2dvLTY0MDI2/Ni5wbmc"
 }]
+
+export const connection = new Connection("https://api.mainnet-beta.solana.com")
+
+export async function  getSupportedTokens() {
+    const TOKEN_ID_MAP: { [mint: string]: string } = {
+        'So11111111111111111111111111111111111111112': 'solana',
+        'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v': 'usd-coin',
+        'Es9vMFrzaC1H6zzggBqqqEgakx4eCnmLmJtZNe5yW3sn': 'tether'
+    };
+
+    if (!LAST_UPDATE || new Date().getTime() - LAST_UPDATE > TOKEN_PRICE_REFRESH_INTERVAL) {
+        try {
+            const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=solana,usd-coin,tether&vs_currencies=usd')
+
+            const data = response.data;
+            // console.log("BRO RES" , data);
+            prices = Object.fromEntries(
+                SUPPORTED_TOKENS.map(t => [t.mint, { price: data[TOKEN_ID_MAP[t.mint]]?.usd ?? 0 }])
+            )
+
+            LAST_UPDATE = Date.now();
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }
+    return SUPPORTED_TOKENS.map(s => ({
+        ...s,
+        price: prices[s.mint]?.price ?? null,
+        
+    }))
+}
